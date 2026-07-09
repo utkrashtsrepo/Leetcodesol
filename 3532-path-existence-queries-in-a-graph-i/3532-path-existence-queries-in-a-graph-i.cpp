eda523aa084b1f6763c4b641238d6985d241640c
utkrashtsrepo <guptautkrasht@gmail.com>
2026-07-09 13:53:19 +0530

    Time: 19 ms (58.36%), Space: 224.1 MB (78.36%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+
+        // comp[i] = connected component of index i
+        vector<int> comp(n, 0);
+
+        for (int i = 1; i < n; i++) {
+            if (abs(nums[i] - nums[i - 1]) <= maxDiff)
+                comp[i] = comp[i - 1];
+            else
+                comp[i] = comp[i - 1] + 1;
+        }
+
+        vector<bool> ans;
+
+        for (auto &q : queries) {
+            ans.push_back(comp[q[0]] == comp[q[1]]);
+        }
+
+        return ans;
+    }
+};
