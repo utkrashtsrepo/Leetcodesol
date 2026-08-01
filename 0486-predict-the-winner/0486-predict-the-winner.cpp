072410c84d15d8ed08c63a2100c198762ba171e5
utkrashtsrepo <guptautkrasht@gmail.com>
2026-08-01 21:23:23 +0530

    Time: 0 ms (100%), Space: 10.3 MB (32.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fun(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != INT_MIN)
+            return dp[i][j];
+
+        int left = nums[i] - fun(i + 1, j, nums, dp);
+        int right = nums[j] - fun(i, j - 1, nums, dp);
+
+        return dp[i][j] = max(left, right);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+
+        return fun(0, n - 1, nums, dp) >= 0;
+    }
+};
